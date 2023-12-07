28691e9301a1a221cc485e71598d63ca1f472996
CARLWALL123 <carlwall123@gmail.com>
2023-12-06 18:33:01 -0600

    update on part one

--- project/src/main/java/thelancers01/project/Controllers/HomeController.java
+++ project/src/main/java/thelancers01/project/Controllers/HomeController.java
@@ -1,12 +1,13 @@
 package thelancers01.project.Controllers;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/")
-    public @ResponseBody String greeting() {
-        return "Hello, World";
+    @GetMapping("/")
+    public String home() {
+        return "homepage";  // This should match the name of your Thymeleaf template
     }
 }
