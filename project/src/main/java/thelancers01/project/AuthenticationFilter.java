2193f7d22a93369c4117cfb03554f0a32fac5322
Cory <standardvr@gmail.com>
2023-12-08 19:34:48 -0600

    Finished Login and Registration logic

---
+++ project/src/main/java/thelancers01/project/AuthenticationFilter.java
@@ -0,0 +1,47 @@
+package thelancers01.project;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerInterceptor;
+import thelancers01.project.controllers.AuthenticationController;
+import thelancers01.project.models.User;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class AuthenticationFilter implements HandlerInterceptor {
+
+    @Autowired
+    AuthenticationController authenticationController;
+
+    private static final List<String> whitelist = Arrays.asList("/homepage", "/login", "/register", "/css", "/images");
+
+    private static boolean isWhitelisted(String path) {
+        for (String pathRoot : whitelist) {
+            if (path.equals("/") || path.startsWith(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+        if (isWhitelisted(request.getRequestURI())) {
+            return true;
+        }
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        if (user != null) {
+            return true;
+        }
+
+        response.sendRedirect("/homepage");
+        return false;
+    }
+
+}
--- project/src/main/java/thelancers01/project/LancersFitness.java
+++ project/src/main/java/thelancers01/project/LancersFitness.java
@@ -2,8 +2,10 @@ package thelancers01.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication( exclude = { SecurityAutoConfiguration.class } )
 public class LancersFitness {
 
     public static void main(String[] args) {
