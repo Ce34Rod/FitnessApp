a5130c90214588d9ba81e54db8efee969465a00f
Ce34Rod <139182790+Ce34Rod@users.noreply.github.com>
2023-12-14 22:15:00 -0600

    Beta addition of create exercise funtion

--- project/src/main/java/thelancers01/project/controllers/CreateExerciseController.java
+++ project/src/main/java/thelancers01/project/controllers/CreateExerciseController.java
@@ -1,42 +1,49 @@
 package thelancers01.project.controllers;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
-import com.example.demo.models.Exercise;
+import thelancers01.project.models.Exercise;
+import thelancers01.project.models.data.ExerciseRepository;
 
+//TODO:  Test for persistence
     @Controller
     @RequestMapping("create")
     public class CreateExerciseController {
 
         public static List<Exercise> exercises = new ArrayList<>();
 
+    @Autowired
+    private ExerciseRepository exerciseRepository;
 
         @GetMapping("exercise")
-        public String ViewCreateAnExercise() {
+        public String ViewCreateAnExercise(Model model) {
+
+            model.addAttribute(new Exercise());
             return "create/exercise";
         }
 
 
         @PostMapping("exercise")
-        public String submitForm(
-                @RequestParam String exerciseName,
-                @RequestParam String exerciseType,
-                @RequestParam String targetMuscles,
-                @RequestParam String exerciseNotes,
+        public String submitForm(@ModelAttribute @Valid Exercise newExercise,
+//                @RequestParam String exerciseName,
+//                @RequestParam String exerciseType,
+//                @RequestParam String targetMuscles,
+//                @RequestParam String exerciseNotes,
                 Model model) {
 
-            exercises.add(new Exercise(exerciseName, exerciseType, targetMuscles, exerciseNotes));
+            exerciseRepository.save(newExercise);
+
+//            exercises.add(new Exercise(exerciseName, exerciseType, targetMuscles, exerciseNotes));
             return "redirect:/userExercises";
         }
 
 
     }
-}
--- project/src/main/java/thelancers01/project/models/Exercise.java
+++ project/src/main/java/thelancers01/project/models/Exercise.java
@@ -1,43 +1,80 @@
 package thelancers01.project.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+@Entity
 public class Exercise {
 
-    private String name;
-    private String type;
-    private String muscle;
-    private String difficulty;
 
-    // Getters and setters
+    @Id
+    @GeneratedValue
+    private int id;
+    public static String name;
+    private static int nextId = 1;
+    private String exerciseName;
+    private String exerciseType;
+    private String targetMuscles;
+    private String exerciseNotes;
+
+    public Exercise(String exerciseName, String exerciseType, String targetMuscles, String exerciseNotes) {
+        this.exerciseName = exerciseName;
+        this.exerciseType = exerciseType;
+        this.targetMuscles = targetMuscles;
+        this.exerciseNotes = exerciseNotes;
+        this.id = nextId;
+        nextId++;
+    }
+
+    public Exercise (){};
+    public int getId() {
+        return id;
+    }
 
     public String getName() {
-        return name;
+        return exerciseName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setName(String exerciseName) {
+        this.exerciseName = exerciseName;
     }
 
-    public String getType() {
-        return type;
+    public String getExerciseType() {
+        return exerciseType;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setExerciseType(String exerciseType) {
+        this.exerciseType = exerciseType;
     }
 
-    public String getMuscle() {
-        return muscle;
+    public String getTargetMuscles() {
+        return targetMuscles;
     }
 
-    public void setMuscle(String muscle) {
-        this.muscle = muscle;
+    public void setTargetMuscles(String targetMuscles) {
+        this.targetMuscles = targetMuscles;
     }
 
-    public String getDifficulty() {
-        return difficulty;
+    public String getExerciseNotes() {
+        return exerciseNotes;
     }
 
-    public void setDifficulty(String difficulty) {
-        this.difficulty = difficulty;
+    public void setExerciseNotes(String exerciseNotes) {
+        this.exerciseNotes = exerciseNotes;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Exercise exercise = (Exercise) o;
+        return id == exercise.id;
     }
 }
