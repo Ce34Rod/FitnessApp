c4beddd9e2362cbfbb5b88344b0fe662f77327ad
Ce34Rod <139182790+Ce34Rod@users.noreply.github.com>
2024-01-04 21:40:31 -0600

    @Cory -Cesar "what I had for the workout functions"

---
+++ project/src/main/java/thelancers01/project/models/Workoutb.java
@@ -0,0 +1,104 @@
+package thelancers01.project.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Workoutb {
+
+
+    @Id
+    @GeneratedValue
+    private int id;
+    public String name;
+    private static int nextId = 1;
+ //   private String workoutName;
+//    private String workoutType;
+//    private String targetMuscles;
+    private String workoutDetails;
+    private String workoutDescription;
+    //private List<Exercise> exerciseList = new ArrayList<>();
+
+
+    public Workoutb(int id, String name, String workoutDetails, String workoutType, String targetMuscles, String workoutDescription, List exerciseList) {
+        this.id = id;
+        this.name = name;
+       // this.workoutName = workoutName;
+        this.workoutDetails = workoutDetails;
+//        this.workoutType = workoutType;
+//        this.targetMuscles = targetMuscles;
+        this.workoutDescription = workoutDescription;
+       // this.exerciseList = exerciseList;
+    }
+
+    public Workoutb(){};
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public String getWorkoutType() {
+//        return workoutType;
+//    }
+//
+//    public void setWorkoutType(String workoutType) {
+//        this.workoutType = workoutType;
+//    }
+//
+//    public String getTargetMuscles() {
+//        return targetMuscles;
+//    }
+//
+//    public void setTargetMuscles(String targetMuscles) {
+//        this.targetMuscles = targetMuscles;
+//    }
+
+    public String getWorkoutDescription() {
+        return workoutDescription;
+    }
+
+    public void setWorkoutDescription(String workoutDescription) {
+        this.workoutDescription = workoutDescription;
+    }
+
+//    public List<Exercise> getExerciseList() {
+//        return exerciseList;
+//    }
+
+//    public void setExerciseList(List<Exercise> exerciseList) {
+//        this.exerciseList = exerciseList;
+//    }
+
+
+    public String getWorkoutDetails() {
+        return workoutDetails;
+    }
+
+    public void setWorkoutDetails(String workoutDetails) {
+        this.workoutDetails = workoutDetails;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Workoutb workoutb = (Workoutb) o;
+        return id == workoutb.id;
+    }
+}
