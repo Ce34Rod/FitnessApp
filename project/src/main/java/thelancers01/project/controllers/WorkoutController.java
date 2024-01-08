16526916c6a37fb95ef2a51ee06dd041a5a703ca
CoryWilk <123584905+CoryWilk@users.noreply.github.com>
2024-01-07 21:41:56 -0600

    Made api key an environment variable for added security

--- project/src/main/java/thelancers01/project/controllers/WorkoutController.java
+++ project/src/main/java/thelancers01/project/controllers/WorkoutController.java
@@ -19,7 +19,7 @@ public class WorkoutController {
 
     @GetMapping("/{workoutId}")
     public String showWorkout(@PathVariable Long workoutId, Model model) {
-        Optional<Workout> optionalWorkout = workoutRepository.findAllById(workoutId);
+        Optional<Workout> optionalWorkout = workoutRepository.findById(workoutId);
 
         if (optionalWorkout.isPresent()) {
             Workout workout = optionalWorkout.get();
