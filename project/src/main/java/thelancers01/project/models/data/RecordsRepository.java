26465612cb9fa73c94df454f453275daabc68657
CoryWilk <123584905+CoryWilk@users.noreply.github.com>
2024-01-10 21:46:18 -0600

    Working on getting the selected api exercises to save to a view

--- project/src/main/java/thelancers01/project/models/data/RecordsRepository.java
+++ project/src/main/java/thelancers01/project/models/data/RecordsRepository.java
@@ -1,9 +1,9 @@
-package thelancers01.project.models.data;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import thelancers01.project.models.DataPoint;
-
-@Repository
-public interface RecordsRepository extends CrudRepository<DataPoint, Integer> {
-}
+//package thelancers01.project.models.data;
+//
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//import thelancers01.project.models.DataPoint;
+//
+//@Repository
+//public interface RecordsRepository extends CrudRepository<DataPoint, Integer> {
+//}
