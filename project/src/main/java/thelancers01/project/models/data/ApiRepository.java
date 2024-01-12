eb428e8693adb71cd120cb7b861ce856c3431b9b
CoryWilk <123584905+CoryWilk@users.noreply.github.com>
2024-01-12 10:40:49 -0600

    Removed extraneous files and made repository safe from duplicate exercises being saved

--- project/src/main/java/thelancers01/project/models/data/ApiRepository.java
+++ project/src/main/java/thelancers01/project/models/data/ApiRepository.java
@@ -12,13 +12,13 @@ public interface ApiRepository extends CrudRepository<ApiExercise, Long> {
 
     List<ApiExercise> findByNameIn(List<String> names);
 
-    @Query("SELECT DISTINCT a.name FROM ApiExercise a")
-    List<String> findAllNames();
+//    @Query("SELECT DISTINCT a.name FROM ApiExercise a")
+//    List<String> findAllNames();
 
     boolean existsByName(String name);
 
-    @Transactional
-    @Modifying
-    @Query(value = "INSERT INTO api_exercise (name, type, muscle, difficulty) VALUES (:name, :type, :muscle, :difficulty)", nativeQuery = true)
-    void saveBulk(@Param("name") String name, @Param("type") String type, @Param("muscle") String muscle, @Param("difficulty") String difficulty);
+//    @Transactional
+//    @Modifying
+//    @Query(value = "INSERT INTO api_exercise (name, type, muscle, difficulty) VALUES (:name, :type, :muscle, :difficulty)", nativeQuery = true)
+//    void saveBulk(@Param("name") String name, @Param("type") String type, @Param("muscle") String muscle, @Param("difficulty") String difficulty);
 }
